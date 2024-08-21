a9b3a596d010bfdbf77f70e44ec87216d1efbb4e
Anagha-lin <linusanagha37@gmail.com>
2024-08-21 22:07:13 +0000

    a

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+// src/utils.js
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School";
+  }
+  return "Holberton School main dashboard";
+}
+
