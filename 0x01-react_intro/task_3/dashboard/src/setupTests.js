f200968711beea98035eb6ca0cf6886aa40bf63c
Anagha-lin <linusanagha37@gmail.com>
2024-08-31 11:48:06 +0000

    a

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,11 +1,9 @@
-// src/setupTests.js
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-// Configure Enzyme with the appropriate adapter
 Enzyme.configure({ adapter: new Adapter() });
-
-// Optionally, you can add a serializer for snapshots
-import { createSerializer } from 'enzyme-to-json';
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
-
